a496d8b3c720a8b7a04e951af0fd18996f14c402
VannXD0 <holilmustofa5@gmail.com>
2026-06-25 15:32:58 +0700

    Update mod.js

--- api/mod.js
+++ api/mod.js
@@ -1,12 +1,7 @@
 export default async function handler(req, res) {
+  // Gist lu yang sudah ada kode base64-nya
   const GIST_URL = "https://gist.githubusercontent.com/VannXD0/e7ba34f1d641d40f209fc0bf899bc6ff/raw/a4ba3b4274f05867eee46fdb9027beda2aa19790/Arc.sh";
   
-  // Langsung fetch dan pipe respon ke res
-  const response = await fetch(GIST_URL);
-  
-  // Set header agar browser/terminal tau ini file script
-  res.setHeader("Content-Type", "text/plain");
-  
-  // Langsung kirim body tanpa diproses apa-apa
-  return response.body.pipe(res);
+  // Redirect 302 adalah cara paling aman agar terminal langsung memanggil Gist
+  return res.redirect(302, GIST_URL);
 }
