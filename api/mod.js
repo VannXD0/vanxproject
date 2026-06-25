8fad802da186bc68e0b52ee8e2fc6f5738c4bc26
VannXD0 <holilmustofa5@gmail.com>
2026-06-25 14:27:10 +0700

    Update mod.js

--- api/mod.js
+++ api/mod.js
@@ -1,13 +1,17 @@
 export default async function handler(req, res) {
-  const G = "aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9WYW5uWEQwL2U3YmEzNGYxZDY0MWQ0MGYyMDlmYzBiZjg5OWJjNjZmL3Jhdy9hNjIzNzk5ZjkwZGU5MTIwZDkwNGNiNWNlNmYwZGNhOWVmMDkwOTM0L0FyYy5zaA==";
-  const U = Buffer.from(G, 'base64').toString('utf8');
-  
+  // Link Gist lu
+  const GIST_URL = "https://gist.githubusercontent.com/VannXD0/e7ba34f1d641d40f209fc0bf899bc66f/raw/a623799f9de9120d904cb5ce6f0dca9ef090934/Arc.sh";
+
   try {
-    const R = await fetch(U);
-    const S = await R.text();
-    const P = Buffer.from(S).toString('base64');
-    res.status(200).send("X123_" + P);
-  } catch (e) {
-    res.status(500).send("ERR");
+    const response = await fetch(GIST_URL);
+    if (!response.ok) throw new Error("Gagal ambil Gist");
+    
+    const script = await response.text();
+
+    // KIRIM TEKS POLOS: Ini kunci biar AxManager gak bingung
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send(script);
+  } catch (err) {
+    res.status(500).send("echo 'Error: Server Gagal Ambil Script'");
   }
 }
