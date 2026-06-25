9b2ca5560ce0a337963655b4085fdf453197472e
VannXD0 <holilmustofa5@gmail.com>
2026-06-25 14:30:03 +0700

    Update mod.js

--- api/mod.js
+++ api/mod.js
@@ -1,12 +1,21 @@
 export default async function handler(req, res) {
-  // Langsung masukkan script di sini, tidak usah fetch dari Gist
-  const script = `
-#!/system/bin/sh
-echo "MOD BERHASIL DIJALANKAN"
-# Masukkan semua perintah mod lu di bawah sini
-# Contoh: input keyevent 3
-  `;
+  const GIST_URL = "https://gist.githubusercontent.com/VannXD0/e7ba34f1d641d40f209fc0bf899bc66f/raw/a623799f9de9120d904cb5ce6f0dca9ef090934/Arc.sh";
 
-  res.setHeader("Content-Type", "text/plain");
-  return res.status(200).send(script);
+  try {
+    // Kita gunakan fetch dengan opsi yang lebih "ramah" buat Vercel serverless
+    const response = await fetch(GIST_URL, {
+      method: 'GET',
+      headers: { 'Cache-Control': 'no-cache' }
+    });
+
+    if (!response.ok) throw new Error("Gist tidak merespon");
+
+    const data = await response.text();
+
+    res.setHeader("Content-Type", "text/plain");
+    return res.status(200).send(data);
+  } catch (err) {
+    // Kalo error, kita kirim pesan yang gak bikin shell AxManager crash
+    res.status(500).send("echo 'Server Gagal Bridge Gist'");
+  }
 }
