e1c0f69dfb1330122fa620f5ed52495c53e9041f
VannXD0 <holilmustofa5@gmail.com>
2026-06-25 14:28:33 +0700

    Update mod.js

--- api/mod.js
+++ api/mod.js
@@ -1,17 +1,12 @@
 export default async function handler(req, res) {
-  // Link Gist lu
-  const GIST_URL = "https://gist.githubusercontent.com/VannXD0/e7ba34f1d641d40f209fc0bf899bc66f/raw/a623799f9de9120d904cb5ce6f0dca9ef090934/Arc.sh";
+  // Langsung masukkan script di sini, tidak usah fetch dari Gist
+  const script = `
+#!/system/bin/sh
+echo "MOD BERHASIL DIJALANKAN"
+# Masukkan semua perintah mod lu di bawah sini
+# Contoh: input keyevent 3
+  `;
 
-  try {
-    const response = await fetch(GIST_URL);
-    if (!response.ok) throw new Error("Gagal ambil Gist");
-    
-    const script = await response.text();
-
-    // KIRIM TEKS POLOS: Ini kunci biar AxManager gak bingung
-    res.setHeader("Content-Type", "text/plain");
-    res.status(200).send(script);
-  } catch (err) {
-    res.status(500).send("echo 'Error: Server Gagal Ambil Script'");
-  }
+  res.setHeader("Content-Type", "text/plain");
+  return res.status(200).send(script);
 }
