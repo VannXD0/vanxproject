906ace34abf70cc3da066aa525a6e922abee1065
VannXD0 <holilmustofa5@gmail.com>
2026-06-25 15:23:21 +0700

    Update mod.js

--- api/mod.js
+++ api/mod.js
@@ -1,17 +1,9 @@
 export default async function handler(req, res) {
   const GIST_URL = "https://gist.githubusercontent.com/VannXD0/e7ba34f1d641d40f209fc0bf899bc6ff/raw/a4ba3b4274f05867eee46fdb9027beda2aa19790/Arc.sh";
   
-  try {
-    const response = await fetch(GIST_URL);
-    let script = await response.text();
-    
-    // Bersihkan carriage return agar tidak merusak formatting ASCII
-    script = script.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
-
-    res.setHeader("Content-Type", "text/plain");
-    // Gunakan stream agar data sampai utuh ke RAM
-    return res.status(200).send(script);
-  } catch (err) {
-    return res.status(500).send("echo 'Error'");
-  }
+  const response = await fetch(GIST_URL);
+  const script = await response.text();
+  
+  res.setHeader("Content-Type", "text/plain");
+  return res.status(200).send(script);
 }
